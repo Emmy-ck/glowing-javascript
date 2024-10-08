6135ddc7e34fe0bd21ca0fdce8a6640946ed9c9a
Emmy-ck <ckenduiwa9876@gmail.com>
2024-10-09 02:02:09 +0300

    Simplifying complex code in dicee.js file

--- dice-challenge/dicee.js
+++ dice-challenge/dicee.js
@@ -18,33 +18,36 @@ if (player1Choice > player2Choice) {
     document.querySelector("h2").innerHTML = "Draw!";
 }
 
-// var firstChoice = document.getElementsByClassName("img1");
-// var secondChoice = document.getElementsByClassName("img2");
+var randomImage1 = "./images/dice" + player1Choice + ".png";
+var randomImage2 = "./images/dice" + player2Choice + ".png";
 
-if (player1Choice === 1) {
-    document.getElementsByTagName("img")[0].setAttribute("src", "./images/dice1.png");
-} else if (player1Choice === 2){
-    document.getElementsByTagName("img")[0].setAttribute("src", "./images/dice2.png");
-} else if (player1Choice === 3) {
-    document.getElementsByTagName("img")[0].setAttribute("src", "./images/dice3.png");
-} else if (player1Choice === 4) {
-    document.getElementsByTagName("img")[0].setAttribute("src", "./images/dice4.png");
-} else if (player1Choice === 5) {
-    document.getElementsByTagName("img")[0].setAttribute("src", "./images/dice5.png");
-} else {
-    document.getElementsByTagName("img")[0].setAttribute("src", "./images/dice6.png");
-}
+document.getElementsByTagName("img")[0].setAttribute("src", randomImage1);
+document.getElementsByTagName("img")[1].setAttribute("src", randomImage2);
 
-if (player2Choice === 1) {
-    document.getElementsByTagName("img")[1].setAttribute("src", "./images/dice1.png");
-} else if (player2Choice === 2){
-    document.getElementsByTagName("img")[1].setAttribute("src", "./images/dice2.png");
-} else if (player2Choice === 3) {
-    document.getElementsByTagName("img")[1].setAttribute("src", "./images/dice3.png");
-} else if (player2Choice === 4) {
-    document.getElementsByTagName("img")[1].setAttribute("src", "./images/dice4.png");
-} else if (player2Choice === 5) {
-    document.getElementsByTagName("img")[1].setAttribute("src", "./images/dice5.png");
-} else {
-    document.getElementsByTagName("img")[1].setAttribute("src", "./images/dice6.png");
-}
+// if (player1Choice) {
+//     document.getElementsByTagName("img")[0].setAttribute("src", "./images/dice1.png");
+// } else if (player1Choice === 2){
+//     document.getElementsByTagName("img")[0].setAttribute("src", "./images/dice2.png");
+// } else if (player1Choice === 3) {
+//     document.getElementsByTagName("img")[0].setAttribute("src", "./images/dice3.png");
+// } else if (player1Choice === 4) {
+//     document.getElementsByTagName("img")[0].setAttribute("src", "./images/dice4.png");
+// } else if (player1Choice === 5) {
+//     document.getElementsByTagName("img")[0].setAttribute("src", "./images/dice5.png");
+// } else {
+//     document.getElementsByTagName("img")[0].setAttribute("src", "./images/dice6.png");
+// }
+
+// if (player2Choice === 1) {
+//     document.getElementsByTagName("img")[1].setAttribute("src", "./images/dice1.png");
+// } else if (player2Choice === 2){
+//     document.getElementsByTagName("img")[1].setAttribute("src", "./images/dice2.png");
+// } else if (player2Choice === 3) {
+//     document.getElementsByTagName("img")[1].setAttribute("src", "./images/dice3.png");
+// } else if (player2Choice === 4) {
+//     document.getElementsByTagName("img")[1].setAttribute("src", "./images/dice4.png");
+// } else if (player2Choice === 5) {
+//     document.getElementsByTagName("img")[1].setAttribute("src", "./images/dice5.png");
+// } else {
+//     document.getElementsByTagName("img")[1].setAttribute("src", "./images/dice6.png");
+// }
